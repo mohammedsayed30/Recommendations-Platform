cd3b7f8e608ef74f47db739ad3221311b621c43d
mohammedsayed30 <mohamedsayedshaaban2022@gmail.com>
2026-08-01 19:26:39 +0300

    add AOP

---
+++ recommendations_BackEnd/src/main/java/recommendations/aop/LoggingAspect.java
@@ -0,0 +1,31 @@
+package recommendations.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("within(recommendations..) && @within(org.springframework.stereotype.Service)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        //run the actual method and receive whatever the actual method return
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info("Executed {} in {} ms", joinPoint.getSignature(), executionTime);
+
+        //return whatever the actual method return to whatever call this method
+        return proceed;
+    }
+
+}
